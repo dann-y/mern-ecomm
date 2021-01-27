c9d0ab4598f43d2d12d0a705fc3430c3f520dce1
Daniel Chu <dannyychuu@gmail.com>
2021-01-27 11:47:30 -0800

    back end user authentication

---
+++ backend/controllers/userController.js
@@ -0,0 +1,89 @@
+import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
+import User from "../models/userModel.js";
+
+//mongoose
+
+//async handler handles errors
+
+// @desc       Auth user & get token
+// @route       POST /api/products
+// @access     Public
+
+const authUser = asyncHandler(async (req, res) => {
+  //retrieve
+  const { email, password } = req.body;
+
+  //find email that matches the email
+  const user = await User.findOne({ email: email });
+
+  //check if user exists & password matches, then respond with data
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error("Invalid email or password");
+  }
+});
+
+// @desc       Get user profile
+// @route       GET /api/users/profile
+// @access     Private
+
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(404);
+    throw new Error("user not found");
+  }
+});
+
+// @desc       Register a new user
+// @route       POST /api/users
+// @access     Public
+
+const registerUser = asyncHandler(async (req, res) => {
+  //retrieve
+  const { name, email, password } = req.body;
+
+  //find email that matches the email
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(400);
+    throw new Error("invalid user data");
+  }
+});
+
+export { authUser, getUserProfile, registerUser };
--- backend/server.js
+++ backend/server.js
@@ -4,12 +4,15 @@ import colors from "colors";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
 
 dotenv.config();
 
 connectDB();
 
 const app = express();
+
+app.use(express.json());
 
 //routes
 
@@ -18,6 +21,7 @@ app.get("/", (req, res) => {
 });
 
 app.use("/api/products", productRoutes);
+app.use("/api/users", userRoutes);
 
 //error handler middleware
 app.use(notFound);
