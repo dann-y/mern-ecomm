5ab02a7991ac16f580dcb9f7c94756c5c4c29c4c
Daniel Chu <dannyychuu@gmail.com>
2021-03-15 21:56:03 -0700

    show orders on screen

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -86,4 +86,14 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
   }
 });
 
-export { addOrderItems, getOrderById, updateOrderToPaid };
+// @desc       Get logged in user orders
+// @route       GET /api/orders/myorders
+// @access     Private
+
+const getMyOrders = asyncHandler(async (req, res) => {
+  // only fetch orders from the logged in user
+  const orders = await Order.find({ user: req.user._id });
+  res.json(orders);
+});
+
+export { addOrderItems, getOrderById, updateOrderToPaid, getMyOrders };
